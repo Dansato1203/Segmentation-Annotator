8c8b735607bdcda2f1dc96b3a2a1de15b7473620
Dan Sato <s19C1054bu@s.chibakoudai.jp>
2021-12-29 17:02:14 +0900

    add function to change the radius of a circle

--- scripts/main.py
+++ scripts/main.py
@@ -15,6 +15,8 @@ def main():
 	files = sorted(glob.glob(input_files_dir + "/*.jpg"))
 	ip = image_process.ColorBase_Annotator(color="green")
 	i = 0
+	rad = 10
+	circle_rad = []
 	click_points = []
 	draw = False
 	window_name = "annotation window"
@@ -39,12 +41,16 @@ def main():
 		cv2.createTrackbar("Saturation_max", window_name, color_mode["s_max"], 255, ip.callback_func)
 		cv2.createTrackbar("Value_min", window_name, color_mode["v_min"], 255, ip.callback_func)
 		cv2.createTrackbar("Value_max", window_name, color_mode["v_max"], 255, ip.callback_func)
-		cv2.createTrackbar("mode_change", window_name, 0, 1, ip.callback_func)
+		cv2.createTrackbar("Radius of circle", window_name, rad, 100, ip.callback_func)
 
 		while True:
 			extract_img = ip.color_extraction(img, hsv_img, **color_mode)
 
-			[cv2.circle(extract_img, point, 10, (0,0,0), thickness=-1) for point in click_points]
+			#[cv2.circle(extract_img, point, rad, (0,0,0), thickness=-1) for point in click_points]
+			for index, point in enumerate(click_points):
+				draw_rad = circle_rad[index]
+				cv2.circle(extract_img, point, draw_rad, (0,0,0), thickness=-1)
+			
 			cv2.imshow(window_name, extract_img)
 
 			color_mode["h_min"] = cv2.getTrackbarPos("Hue_min", window_name)
@@ -53,33 +59,45 @@ def main():
 			color_mode["s_max"] = cv2.getTrackbarPos("Saturation_max", window_name)
 			color_mode["v_min"] = cv2.getTrackbarPos("Value_min", window_name)
 			color_mode["v_max"] = cv2.getTrackbarPos("Value_max", window_name)
-			mode = cv2.getTrackbarPos("mode_change", window_name)
+			rad = cv2.getTrackbarPos("Radius of circle", window_name)
 
 			if mouseData.getEvent() == cv2.EVENT_LBUTTONDOWN:
 				draw = True
 				click_points.append(mouseData.getPos())
+				circle_rad.append(rad)
 			if mouseData.getEvent() == cv2.EVENT_LBUTTONUP:
 				draw = False
 			if mouseData.getEvent() == cv2.EVENT_MOUSEMOVE and draw:
 				#print(mouseData.getPos())
 				if draw:
 					click_points.append(mouseData.getPos())
+					circle_rad.append(rad)
 			if mouseData.getEvent() == cv2.EVENT_RBUTTONDOWN:
 				extract_img = ip.reset_drawing(img, extract_img, mouseData.getPos()[0], mouseData.getPos()[1], 10)
 
 			k = cv2.waitKey(1)
 			if k == ord('n'):
 				click_points = list()
+				circle_rad = list()
 				i = i+1
 				break
 			if k == ord('b'):
 				click_points = list()
+				circle_rad = list()
 				i = i-1
 				break
 			if k == ord('z'):
 				click_points.pop()
+				circle_rad.pop()
 			if k == 27:
 				sys.exit()
+		
+		threshed_img = ip.image_thresholding(extract_img)
+		fdir_name = os.path.split(files[i-1])
+		print(fdir_name)
+		file_name = os.path.splitext(fdir_name[-1])
+		print(file_name)
+		threshed_img.save(file_name[0] + "_label.png")
 
 	print("-------- finish --------")
 	cv2.destroyAllWindows()
