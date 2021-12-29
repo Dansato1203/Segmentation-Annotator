1f550ff1184e6c12bbcff42fb05b4808ef0abfd3
Dan Sato <s19C1054bu@s.chibakoudai.jp>
2021-12-29 11:14:20 +0900

    add mouse_event class

---
+++ scripts/mouse_event.py
@@ -0,0 +1,32 @@
+import cv2
+
+class MouseEvent:
+    def __init__(self, input_img_name):
+        #マウス入力用のパラメータ
+        self.mouseEvent = {"x":None, "y":None, "event":None, "flags":None}
+        #マウス入力の設定
+        cv2.setMouseCallback(input_img_name, self.__CallBackFunc, None)
+
+    #コールバック関数
+    def __CallBackFunc(self, eventType, x, y, flags, userdata):
+
+        self.mouseEvent["x"] = x
+        self.mouseEvent["y"] = y
+        self.mouseEvent["event"] = eventType
+        self.mouseEvent["flags"] = flags
+
+    #マウス入力用のパラメータを返すための関数
+    def getData(self):
+        return self.mouseEvent
+
+    #マウスイベントを返す関数
+    def getEvent(self):
+        return self.mouseEvent["event"]
+
+    #マウスフラグを返す関数
+    def getFlags(self):
+        return self.mouseEvent["flags"]
+
+    #xとyの座標を返す関数
+    def getPos(self):
+        return (self.mouseEvent["x"], self.mouseEvent["y"])
