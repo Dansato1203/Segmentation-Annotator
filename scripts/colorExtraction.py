a9421c101c8c9a910794223207ef12951b71be0c
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2023-11-01 23:52:16 +0900

    Changes to integrate magicWand.py

--- scripts/colorExtraction.py
+++ scripts/colorExtraction.py
@@ -1,16 +1,24 @@
 #! /user/bin/env python3
 
+import os
 import cv2
 from PIL import Image, ImageDraw
 import numpy as np
 import glob
 
 class ColorExtraction:
-	def __init__(self):
+	def __init__(self, file_num):
 		self.color_mode = "none"
 		self.color_param = {"h_min": 0, "h_max": 180, "s_min": 0, "s_max": 255, "v_min": 0, "v_max": 255}
 		self.colors = {"none": self.color_param}
 		self.saved_colors = {}
+		self.window_name = "annotation window"
+		self.draw = False
+		self.click_points = []
+		self.circle_rad = []
+		self.rad = 10
+		self.file_number = file_num
+
 
 	def callback_func(self, val):
 		pass
@@ -66,4 +74,101 @@ class ColorExtraction:
 			print(f"Loaded color parameters from key {key}")
 		else:
 			print(f"No color parameters saved for key {key}")
+
+	def save_image(self, img_path):
+		# パスを分割
+		directory_path , file_name = os.path.split(img_path)
+		img_name, _ = os.path.splitext(file_name)
+
+		# ラベル用のディレクトリを作成
+		label_directory = os.path.join(directory_path, 'labels')
+		if not os.path.exists(label_directory):
+			os.mkdir(label_directory)
+
+		annotate_img_name = os.path.join(label_directory, (img_name + '_label.png'))
+		print(annotate_img_name)
+		cv2.imwrite(annotate_img_name, self.extraction_img)
+
+	def __mouse_callback(self, event, x, y, flags, param):
+		if event == cv2.EVENT_LBUTTONDOWN:
+			self.draw = True
+			self.click_points.append((x, y))
+			self.circle_rad.append(self.rad)
+		elif event == cv2.EVENT_LBUTTONUP:
+			self.draw = False
+		elif event == cv2.EVENT_MOUSEMOVE and self.draw:
+			self.click_points.append((x, y))
+			self.circle_rad.append(self.rad)
+		elif event == cv2.EVENT_RBUTTONDOWN:
+			self.reset_drawing(self.img, self.extract_img, x, y, 10)
+
+	def run(self, img_path):
+		self.img = cv2.imread(img_path)
+		self.hsv_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+		cv2.namedWindow(self.window_name)
+		cv2.setMouseCallback(self.window_name, self.__mouse_callback)
+
+		# トラックバーの設定
+		color_mode = self.get_hsv_param()
+		cv2.createTrackbar("Hue_min", self.window_name, color_mode["h_min"], 180, self.callback_func)
+		cv2.createTrackbar("Hue_max", self.window_name, color_mode["h_max"], 180, self.callback_func)
+		cv2.createTrackbar("Saturation_min", self.window_name, color_mode["s_min"], 255, self.callback_func)
+		cv2.createTrackbar("Saturation_max", self.window_name, color_mode["s_max"], 255, self.callback_func)
+		cv2.createTrackbar("Value_min", self.window_name, color_mode["v_min"], 255, self.callback_func)
+		cv2.createTrackbar("Value_max", self.window_name, color_mode["v_max"], 255, self.callback_func)
+		cv2.createTrackbar("Radius of circle", self.window_name, self.rad, 100, self.callback_func)
+
+		save_mode = False
+		load_mode = False
+
+		while True:
+			self.extract_img = self.color_extraction(self.img, self.hsv_img)
+			for index, point in enumerate(self.click_points):
+				draw_rad = self.circle_rad[index]
+				cv2.circle(self.extract_img, point, draw_rad, (0,0,0), thickness=-1)
+			cv2.imshow("origin_image", self.img)
+			cv2.imshow(self.window_name, self.extract_img)
+
+			color_mode["h_min"] = cv2.getTrackbarPos("Hue_min", self.window_name)
+			color_mode["h_max"] = cv2.getTrackbarPos("Hue_max", self.window_name)
+			color_mode["s_min"] = cv2.getTrackbarPos("Saturation_min", self.window_name)
+			color_mode["s_max"] = cv2.getTrackbarPos("Saturation_max", self.window_name)
+			color_mode["v_min"] = cv2.getTrackbarPos("Value_min", self.window_name)
+			color_mode["v_max"] = cv2.getTrackbarPos("Value_max", self.window_name)
+			self.rad = cv2.getTrackbarPos("Radius of circle", self.window_name)
+
+			key = cv2.waitKey(1) & 0xFF
+			if key == ord('n'):
+				self.file_number = self.file_number+1
+				self.save_image(img_path)
+				break
+			elif key == ord('b'):
+				self.file_number = self.file_number-1
+				self.save_image(img_path)
+				break
+			elif key == ord('s'):
+				save_mode = True
+				load_mode = False
+			elif key == ord('l'):
+				load_mode = True
+				save_mode = False
+			elif ord('0') <= key <= ord('9') and (save_mode or load_mode):  # 数字キーが押された場合
+				key_num = chr(key)
+				if save_mode:
+					self.save_color(key_num)
+					save_mode = False
+				elif load_mode:
+					self.load_color(key_num)
+					# トラックバーの位置を更新
+					cv2.setTrackbarPos("Hue_min", self.window_name, self.color_param["h_min"])
+					cv2.setTrackbarPos("Hue_max", self.window_name, self.color_param["h_max"])
+					cv2.setTrackbarPos("Saturation_min", self.window_name, self.color_param["s_min"])
+					cv2.setTrackbarPos("Saturation_max", self.window_name, self.color_param["s_max"])
+					cv2.setTrackbarPos("Value_min", self.window_name, self.color_param["v_min"])
+					cv2.setTrackbarPos("Value_max", self.window_name, self.color_param["v_max"])
+					load_mode = False
+			elif key == 27:  # ESC key
+				break
+
+		cv2.destroyAllWindows()
 
--- scripts/main.py
+++ scripts/main.py
@@ -2,120 +2,34 @@
 
 import cv2
 import numpy as np
-import os
-import sys
 import glob
 import colorExtraction
-import mouseEvent
 
 def main():
 	print("input your image dir : ", end='')
 	input_files_dir = input()
 	files = sorted(glob.glob(input_files_dir + "/*.jpg"))
-	ip = colorExtraction.ColorExtraction()
-	i = 0
-	rad = 10
-	circle_rad = []
-	click_points = []
-	draw = False
-	window_name = "annotation window"
-
-	save_mode = False
-	load_mode = False
+	file_num = 0
 
 	while True:
-		img = cv2.imread(files[i])
-		print(files[i])
-		hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-		color_mode = ip.get_hsv_param()
-		extract_img = ip.color_extraction(img, hsv_img)
-		cv2.imshow(window_name, extract_img)
-		cv2.imshow("origin_img", img)
+		img_path = files[file_num]
+		print(img_path)
 
-		mouseData = mouseEvent.MouseEvent(window_name)
-
-		# トラックバーの設定
-		cv2.createTrackbar("Hue_min", window_name, color_mode["h_min"], 180, ip.callback_func)
-		cv2.createTrackbar("Hue_max", window_name, color_mode["h_max"], 180, ip.callback_func)
-		cv2.createTrackbar("Saturation_min", window_name, color_mode["s_min"], 255, ip.callback_func)
-		cv2.createTrackbar("Saturation_max", window_name, color_mode["s_max"], 255, ip.callback_func)
-		cv2.createTrackbar("Value_min", window_name, color_mode["v_min"], 255, ip.callback_func)
-		cv2.createTrackbar("Value_max", window_name, color_mode["v_max"], 255, ip.callback_func)
-		cv2.createTrackbar("Radius of circle", window_name, rad, 100, ip.callback_func)
-
-		while True:
-			extract_img = ip.color_extraction(img, hsv_img)
-			for index, point in enumerate(click_points):
-				draw_rad = circle_rad[index]
-				cv2.circle(extract_img, point, draw_rad, (0,0,0), thickness=-1)
-			cv2.imshow(window_name, extract_img)
-
-			color_mode["h_min"] = cv2.getTrackbarPos("Hue_min", window_name)
-			color_mode["h_max"] = cv2.getTrackbarPos("Hue_max", window_name)
-			color_mode["s_min"] = cv2.getTrackbarPos("Saturation_min", window_name)
-			color_mode["s_max"] = cv2.getTrackbarPos("Saturation_max", window_name)
-			color_mode["v_min"] = cv2.getTrackbarPos("Value_min", window_name)
-			color_mode["v_max"] = cv2.getTrackbarPos("Value_max", window_name)
-			rad = cv2.getTrackbarPos("Radius of circle", window_name)
-
-			if mouseData.getEvent() == cv2.EVENT_LBUTTONDOWN:
-				draw = True
-				click_points.append(mouseData.getPos())
-				circle_rad.append(rad)
-			if mouseData.getEvent() == cv2.EVENT_LBUTTONUP:
-				draw = False
-			if mouseData.getEvent() == cv2.EVENT_MOUSEMOVE and draw:
-				if draw:
-					click_points.append(mouseData.getPos())
-					circle_rad.append(rad)
-			if mouseData.getEvent() == cv2.EVENT_RBUTTONDOWN:
-				extract_img = ip.reset_drawing(img, extract_img, mouseData.getPos()[0], mouseData.getPos()[1], 10)
-
-			k = cv2.waitKey(1)
-			if k == ord('n'):
-				click_points = list()
-				circle_rad = list()
-				i = i+1
-				break
-			elif k == ord('b'):
-				click_points = list()
-				circle_rad = list()
-				i = i-1
-				break
-			elif k == ord('z'):
-				click_points.pop()
-				circle_rad.pop()
-			elif k == ord('s'):
-				save_mode = True
-				load_mode = False
-			elif k == ord('l'):
-				load_mode = True
-				save_mode = False
-			elif ord('0') <= k <= ord('9') and (save_mode or load_mode):  # 数字キーが押された場合
-				key_num = chr(k)
-				if save_mode:
-					ip.save_color(key_num)
-					save_mode = False
-				elif load_mode:
-					ip.load_color(key_num)
-					# トラックバーの位置を更新
-					cv2.setTrackbarPos("Hue_min", window_name, ip.color_param["h_min"])
-					cv2.setTrackbarPos("Hue_max", window_name, ip.color_param["h_max"])
-					cv2.setTrackbarPos("Saturation_min", window_name, ip.color_param["s_min"])
-					cv2.setTrackbarPos("Saturation_max", window_name, ip.color_param["s_max"])
-					cv2.setTrackbarPos("Value_min", window_name, ip.color_param["v_min"])
-					cv2.setTrackbarPos("Value_max", window_name, ip.color_param["v_max"])
-					load_mode = False
-			elif k == 27:
-				sys.exit()
-
-		threshed_img = ip.image_thresholding(extract_img)
-		fdir_name = os.path.split(files[i-1])
-		file_name = os.path.splitext(fdir_name[-1])
-		threshed_img.save(file_name[0] + "_label.png")
+		# ColorExtraction インスタンスを作成し、run メソッドを呼び出す
+		ip = colorExtraction.ColorExtraction(file_num)
+		ip.run(img_path)
+		file_num = ip.file_number
+#		k = cv2.waitKey(1) & 0XFF
+#		if k == ord('n'):
+#			file_num = file_num+1 if file_num < len(files) - 1 else file_num
+#		elif k == ord('b'):
+#			file_num = file_num-1 if file_num > 0 else i
+#		elif k == 27:  # ESC key
+#			sys.exit()
 
 	print("-------- finish --------")
 	cv2.destroyAllWindows()
 
 if __name__ == "__main__":
 	main()
+
