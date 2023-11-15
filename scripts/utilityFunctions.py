1df1c9b562c7c3a84b23bf5af35355cdb6d7ce5d
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2023-11-16 00:12:58 +0900

    Add save function

--- scripts/utilityFunctions.py
+++ scripts/utilityFunctions.py
@@ -1,7 +1,21 @@
 #! /usr/bin/env python3
 
+import os
 import numpy as np
 import cv2
+
+def save_processed_image(image_path, segmented_image):
+	# 保存先ディレクトリのパスを作成
+	label_dir = os.path.join(os.path.dirname(image_path), 'labels')
+	if not os.path.exists(label_dir):
+		os.makedirs(label_dir)
+
+	# 保存するファイル名を決定
+	base_name = os.path.splitext(os.path.basename(image_path))[0]
+	save_path = os.path.join(label_dir, f"{base_name}_label.png")
+
+	# 画像を保存
+	cv2.imwrite(save_path, segmented_image)
 
 def remove_segment_at_point(segmented_image, point, radius=10):
 	"""
